52e6851905fbb46604491ca81f1616e05e394375
mdeasindev <easin55474@gmail.com>
2020-03-21 15:59:57 +0600

    modify footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return(
         <footer className="pt-3 pb-3 mt-5">
-            <p><a href="https://github.com/mdeasindev" rel="noopener noreferrer" target="_blank">Md Easin</a> | All right resevered</p>
+            <p><a href="https://github.com/mdeasindev" rel="noopener noreferrer" target="_blank">Md Easin</a> | All Right Resevered</p>
         </footer>
     )
 }
